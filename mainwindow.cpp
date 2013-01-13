356192a5dcc03514fc2d7fa124103d800a4c5bbc
gitarooLegend <Dragon69315@yahoo.com>
2013-01-13 00:58:33 -0500

    Changed object names.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,14 +15,14 @@ MainWindow::~MainWindow()
 void MainWindow::setupUi()
 {
     _main = new QWidget(this);
-    _main->setObjectName(QString("MainWindow::_centralWidget"));
+    _main->setObjectName(QString("MainWindow::_main"));
 
     _mainLayout = new QVBoxLayout();
     _mainLayout->setObjectName(QString("MainWindow::_mainLayout"));
     _main->setLayout(_mainLayout);
 
     _screen = new MyGLDraw(_main);
-    _screen->setObjectName(QString("MainWindow::_screenWidget"));
+    _screen->setObjectName(QString("MainWindow::_screen"));
     _mainLayout->addWidget(_screen);
 
     setCentralWidget(_main);
