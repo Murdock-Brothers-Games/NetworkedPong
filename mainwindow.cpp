29d24a96ad2062a1032abefeaf64cc2ee4945e1b
gitarooLegend <Dragon69315@yahoo.com>
2013-01-13 00:57:19 -0500

    Added construction of main window.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,29 @@
 #include "mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent):
+QMainWindow(parent),
+  _screen(NULL)
 {
+    setupUi();
 }
 
 MainWindow::~MainWindow()
 {
     
+}
+
+void MainWindow::setupUi()
+{
+    _main = new QWidget(this);
+    _main->setObjectName(QString("MainWindow::_centralWidget"));
+
+    _mainLayout = new QVBoxLayout();
+    _mainLayout->setObjectName(QString("MainWindow::_mainLayout"));
+    _main->setLayout(_mainLayout);
+
+    _screen = new MyGLDraw(_main);
+    _screen->setObjectName(QString("MainWindow::_screenWidget"));
+    _mainLayout->addWidget(_screen);
+
+    setCentralWidget(_main);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,14 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVBoxLayout>
+
+#include "mygldraw.h"
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
     
 public:
+
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private:
+    void setupUi();
+
+    MyGLDraw* _screen;
+
+    QWidget* _main;
+    QVBoxLayout* _mainLayout;
+
 };
 
 #endif // MAINWINDOW_H
